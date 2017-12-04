31a87faa0d70a17c6cb7def62115ee1fdad0bb3e
Aaron <aaronmulvaney@gmail.com>
2017-12-04 21:06:59 +0000

    Part 1 complete

--- runtest.sh
+++ runtest.sh
@@ -13,7 +13,7 @@ do
         completions=`wc -l < synthetic.dat`
         #set variable idleTime to be equal to the system idle time during the loadtest
         idleTime=`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
-        #output the number of completions, number of concurrent users and the system idle time for each loadtest
+        #output the number of completions, number of concurrent users and the system idle time for each loa$
         echo -e "$completions\t$i\t$idleTime" >> results.dat
         #kill the loadtest process
         pkill loadtest  
