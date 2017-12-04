91c575c2d06cdb6642ab2ac0ab39ad001152d4b1
root <root@localhost.localdomain>
2017-12-04 02:08:40 +0000

    C0 taken from number of lines in synthetic.dat

--- runtest.sh
+++ runtest.sh
@@ -6,15 +6,15 @@ completions=0
 #for loop to increase the number of concurrent users every loop
 for i in {1..50}
 do
-	#run the loadtest in the background for 5 seconds
-	./loadtest $i &
-	sleep 5
-	#increment the number of completions
-	completions=`expr $completions + 1`
-	#set variable idleTime to be equal to the system idle time during the loadtest
-	idleTime=`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
-	#output the number of completions, number of concurrent users and the system idle time for each loadtest
-	echo -e "$completions\t$i\t$idleTime" >> results.dat
-	#kill the loadtest process
-	pkill loadtest	
+        #run the loadtest in the background for 5 seconds
+        ./loadtest $i &
+        sleep 5
+        #set the number of completions since first loadtest
+        completions=`wc -l < synthetic.dat`
+        #set variable idleTime to be equal to the system idle time during the loadtest
+        idleTime=`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
+        #output the number of completions, number of concurrent users and the system idle time for each loadtest
+        echo -e "$completions\t$i\t$idleTime" >> results.dat
+        #kill the loadtest process
+        pkill loadtest  
 done
