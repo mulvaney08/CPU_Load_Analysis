be5bc5c5acac4bf2d1b876b817e965f16618cf1b
Aaron <aaronmulvaney@gmail.com>
2017-12-14 17:51:19 +0000

    Done

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#All work completed by Aaron Mulvaney - X00118910
 #Echo the title for the output table to file named results.dat
 echo -e "CO\tN\tidle" > results.dat
 
@@ -9,17 +10,16 @@ echo "This may take a few minutes..."
 #for loop to increase the number of concurrent users every loop starting at 1 up to 50
 for i in {1..50}
 do
-        #run the loadtest in the background for 5 seconds
+        #run the loadtest in the background
         ./loadtest $i &
-        sleep 5
-        #set variable idleTime to be equal to the system idle time during the loadtest
-        idleTime=`mpstat -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
+        #set variable idleTime to be equal to the system idle time after the loadtest has ran for 5 seconds
+        idleTime=`mpstat 5 1 -o JSON | jq '.sysstat.hosts[0].statistics[0]."cpu-load"[0].idle'`
+        #kill the loadtest process
+        pkill loadtest
         #set the number of completions for this loadtest
         completions=`wc -l < synthetic.dat`
         #output the number of completions, number of concurrent users and the system idle time for each loadtest
         echo -e "$completions\t$i\t$idleTime" >> results.dat
-        #kill the loadtest process
-	pkill loadtest
 done
 
 echo "Loadtest complete for 1..50 users, please see results.dat for the results of this test"
